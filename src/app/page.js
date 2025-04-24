209ddaaebef71a44851753b060b4c63713e2bf2a
cakmakg <cakmak4834@gmail.com>
2025-04-24 20:26:12 +0200

    intro-routing up

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import Cards from "./components/Cards"  
+import Cards from "../components/Cards"  
 import {useRouter} from "next/navigation"
 
 export default function Home() {
